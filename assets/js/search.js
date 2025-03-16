06537ba0c9729620d57b3de205db66240d52775a
mnrst <minrst@live.jp>
2025-03-16 14:03:31 +0900

    update

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,56 +1,33 @@
 jQuery(function() {
-    // Initalize lunr with the fields it will be searching on. I've given title
-    // a boost of 10 to indicate matches on this field are more important.
-    window.idx = lunr(function () {
-      this.field('id');
-      this.field('title', { boost: 10 });
-      this.field('date');
-      this.field('category');
-    });
-  
-    // Download the data from the JSON file we generated
-    window.data = $.getJSON('/search_data.json');
-  
-    // Wait for the data to load and add it to lunr
-    window.data.then(function(loaded_data){
-      $.each(loaded_data, function(index, value){
-        window.idx.add(
-          $.extend({ "id": index }, value)
-        );
-      });
-    });
-  
-    // Event when the form is submitted
-    $("#site_search").submit(function(){
-        event.preventDefault();
-        var query = $("#search_box").val(); // Get the value for the text field
-        var results = window.idx.search(query); // Get lunr to perform a search
-        display_search_results(results); // Hand the results off to be displayed
-    });
-  
-    function display_search_results(results) {
-      var $search_results = $("#search_results");
-  
-      // Wait for data to load
-      window.data.then(function(loaded_data) {
-  
-        // Are there any results?
-        if (results.length) {
-          $search_results.empty(); // Clear any old results
-  
-          // Iterate over the results
-          results.forEach(function(result) {
-            var item = loaded_data[result.ref];
-  
-            // Build a snippet of HTML for this result
-            var appendString = '<li><a href="' + item.url + '">' + item.title + '</a> ' + item.date.slice(0, 10) + '</li>';
-  
-            // Add it to the results
-            $search_results.append(appendString);
-          });
-        } else {
-          $search_results.html('<li>No results found</li>');
+    $.getJSON('/search_data.json', function(data) {
+        const options = {
+            keys: ['title', 'content'],
+            includeScore: true,
+            threshold: 0.3
+        };
+        const fuse = new Fuse(data, options);
+
+        $("#site_search").submit(function(event) {
+            event.preventDefault();
+            const query = $("#search_box").val();
+            const results = fuse.search(query);
+
+            display_search_results(results);
+        });
+
+        // 検索結果表示関数
+        function display_search_results(results) {
+            const $search_results = $("#search_results");
+            $search_results.empty(); // 古い結果をクリア
+
+            if (results.length) {
+                results.forEach(({ item }) => {
+                    const appendString = `<li><a href="${item.url}">${item.title}</a> ${item.date.slice(0, 10)}</li>`;
+                    $search_results.append(appendString);
+                });
+            } else {
+                $search_results.html('<li>見つかりませんでした</li>');
+            }
         }
-      });
-    }
-  });
+    });
+});
